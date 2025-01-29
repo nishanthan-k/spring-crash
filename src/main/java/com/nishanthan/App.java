91578e4a21ad9ba26b5a6190317529ffb1bb2ff7
nishanthan-k <krn.nishanthan@gmail.com>
2025-01-29 13:57:27 +0530

    closed the context(memory leakage)

--- src/main/java/com/nishanthan/App.java
+++ src/main/java/com/nishanthan/App.java
@@ -1,12 +1,13 @@
 package com.nishanthan;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        BeanFactory factory = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) factory.getBean("alien");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Alien obj = (Alien) context.getBean("alien");
         obj.code();
+
+        context.close();
     }
 }
